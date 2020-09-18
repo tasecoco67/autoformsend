b00c619b70172307a0b565c70ceed71079121b1e
Taisei Katafuchi <tasecoco67@outlook.jp>
2020-09-18 18:19:04 +0900

    second commit

--- setting.py
+++ setting.py
@@ -9,3 +9,8 @@ load_dotenv(dotenv_path)
 
 MAIL = os.environ.get("MAIL_ADDRESS")
 PSW = os.environ.get("UTOKYO_PSW")
+URL = os.environ.get("URL")
+AREA = os.environ.get("AREA")
+TARGET =os.environ.get("TARGET")
+TEMP = os.environ.get("TEMP")
+ADD = os.environ.get("ADD")
