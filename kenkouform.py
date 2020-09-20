20e51ed7312583072f8043d0bef1b8b518432ac1
Taisei Katafuchi <tasecoco67@outlook.jp>
2020-09-20 10:47:27 +0900

    forth commit

--- kenkouform.py
+++ kenkouform.py
@@ -1,5 +1,3 @@
-import ibm_db
-import ibm_db_dbi
 import setting
 
 from selenium import webdriver
@@ -9,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 driver = webdriver.Chrome()
-
 
 #driver.get("https://www.u-tokyo.ac.jp/ja/general/healthcheck.html")
 driver.get(setting.URL)
@@ -21,13 +18,16 @@ driver.find_element_by_xpath(r"//*[@id='passwordInput']").send_keys(setting.PSW)
 driver.find_element_by_xpath(r"//*[@id='submitButton']").click()
 driver.find_element_by_xpath(r"//*[@id='idBtn_Back']").click()
 time.sleep(5)
-driver.find_element_by_xpath(setting.ADD).click()
+driver.find_element_by_xpath("//input[@value='ECCSクラウドメール(共通ID@g.ecc.u-tokyo.ac.jp)宛に送信']"
+).click()
 import datetime
 weekday = datetime.date.today().weekday()
 if str(weekday) == "0"or"1"or"2"or"3"or"4":
-    driver.find_element_by_xpath(setting.AREA).click()
-driver.find_element_by_class_name('office-form-question-textbox').send_keys(setting.TARGET)
-driver.find_element_by_xpath(setting.TEMP).click()
+    driver.find_element_by_xpath("//input[@value='本郷地区／Hongo Area']").click()
+driver.find_element_by_class_name('office-form-question-textbox').send_keys("農学部三号館")
+driver.find_element_by_xpath("//input[@value='37.0度未満／Less than 37.0 degrees Celsius']"
+).click()
 time.sleep(5)
 driver.find_element_by_xpath("//input[@value='いいえ／No']").click()
+
 #//*[@id="form-container"]/div/div/div/div/div[3]/div[3]/div[1]/button/div
--- setting.py
+++ setting.py
@@ -1,16 +1,9 @@
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# .envファイルの内容を読み込みます
+load_dotenv()
 
 MAIL = os.environ.get("MAIL_ADDRESS")
 PSW = os.environ.get("UTOKYO_PSW")
 URL = os.environ.get("URL")
-AREA = os.environ.get("AREA")
-TARGET =os.environ.get("TARGET")
-TEMP = os.environ.get("TEMP")
-ADD = os.environ.get("ADD")
