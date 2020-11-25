a77740066dc610bc5206fe56a2ec665ebd135a3b
tasecoco67 <>
2020-11-25 16:47:58 +0900

    tenth commit

--- kenkouform.py
+++ kenkouform.py
@@ -8,18 +8,18 @@ import time
 
 driver = webdriver.Chrome()
 
-#driver.get("https://www.u-tokyo.ac.jp/ja/general/healthcheck.html")
 driver.get(setting.URL)
-driver.implicitly_wait(10)
-#driver.find_element_by_xpath(r"//*[@id='content']/div[2]/div[1]/div/div/div[1]/a").click()
+time.sleep(2)
 driver.find_element_by_xpath(r"//*[@id='i0116']").send_keys(setting.MAIL)
 driver.find_element_by_xpath(r"//*[@id='idSIButton9']").click()
-driver.find_element_by_xpath(r"//*[@id='passwordInput']").send_keys(setting.PSW)
+time.sleep(3)
+driver.find_element_by_name("Password").send_keys(setting.PSW)
+time.sleep(1)
 driver.find_element_by_xpath(r"//*[@id='submitButton']").click()
+time.sleep(1)
 driver.find_element_by_xpath(r"//*[@id='idBtn_Back']").click()
-time.sleep(5)
-driver.find_element_by_xpath("//input[@value='ECCSクラウドメール(共通ID@g.ecc.u-tokyo.ac.jp)宛に送信']"
-).click()
+time.sleep(3)
+driver.find_element_by_xpath("//input[@value='ECCSクラウドメール(共通ID@g.ecc.u-tokyo.ac.jp)宛に送信']").click()
 import datetime
 weekday = datetime.date.today().weekday()
 if str(weekday) == "0"or"1"or"2"or"3"or"4":
@@ -27,6 +27,6 @@ if str(weekday) == "0"or"1"or"2"or"3"or"4":
 driver.find_element_by_class_name('office-form-question-textbox').send_keys("農学部三号館")
 driver.find_element_by_xpath("//input[@value='37.0度未満／Less than 37.0 degrees Celsius']"
 ).click()
-time.sleep(5)
+time.sleep(3)
 driver.find_element_by_xpath("//input[@value='いいえ／No']").click()
 driver.find_element_by_class_name('office-form-bottom-button').click()
