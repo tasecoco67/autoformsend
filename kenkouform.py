a8c4358843ea3876790f66f6ccd9cefe3d3131f2
tasecoco67 <>
2020-11-25 20:36:13 +0900

    16th

--- kenkouform.py
+++ kenkouform.py
@@ -12,7 +12,7 @@ driver.get("https://login.microsoftonline.com/common/oauth2/authorize?response_m
 time.sleep(2)
 driver.find_element_by_xpath(r"//*[@id='i0116']").send_keys("MAIL_ADDRESS")
 driver.find_element_by_xpath(r"//*[@id='idSIButton9']").click()
-time.sleep(3)
+time.sleep(5)
 driver.find_element_by_xpath(r'//*[@id="passwordInput"]').send_keys("UTOKYO_PSW")
 time.sleep(1)
 driver.find_element_by_xpath(r"//*[@id='submitButton']").click()
@@ -27,6 +27,6 @@ if str(weekday) == "0"or"1"or"2"or"3"or"4":
 driver.find_element_by_class_name('office-form-question-textbox').send_keys("PLACE_NAME")
 driver.find_element_by_xpath("//input[@value='37.0度未満／Less than 37.0 degrees Celsius']"
 ).click()
-time.sleep(3)
+time.sleep(5)
 driver.find_element_by_xpath("//input[@value='いいえ／No']").click()
 driver.find_element_by_class_name('office-form-bottom-button').click()
